e23b2ec682caa95b50c5b9e2d0bea48a6c743573
Sunjoli Aggarwal <sunjoli.aggarwal@gmail.com>
2019-02-16 19:35:37 -0600

    Prefab folder and waypoints folder

---
+++ Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0; 
+
+    void Start()
+    {
+        target = Waypoints.points[0]; 
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed); 
+    }
+}
